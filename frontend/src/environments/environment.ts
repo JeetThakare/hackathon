55066b902394254465070e06c880f9c6cde867e0
Pranayjeet Thakare <jeetthakare@hotmail.com>
2019-04-21 00:29:38 -0700

    initial commit

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -7,11 +7,11 @@ export const environment = {
   production: false,
   backendHostUrl: "http://localhost:8081",
   firebase: {
-    apiKey: "AIzaSyBdTAbnhQ0aEHHNj9Tr1xp3tHjje7ZowM0",
-    authDomain: "cuez-fb8d0.firebaseapp.com",
-    databaseURL: "https://cuez-fb8d0.firebaseio.com",
-    projectId: "cuez-fb8d0",
-    storageBucket: "cuez-fb8d0.appspot.com",
-    messagingSenderId: "543348701259"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
